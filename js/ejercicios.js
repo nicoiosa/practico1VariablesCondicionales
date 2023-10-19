742a8d4a4ec609f6b5a1e5d7926a378d1617fb7c
nicoIosa <nicolasiosa@hotmail.com>
2023-10-19 08:57:44 -0300

    ejercicio terminado

---
+++ js/ejercicios.js
@@ -0,0 +1,17 @@
+// Ejercicio 1
+let numero1 = 10;
+let numero2 = 7;
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 / numero2);
+console.log(numero1 * numero2);
+// Ejercicio 2
+let nombre = "Ryan";
+let apellido = "Gosling";
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+// Ejercicio 3
+let gradosCelsius = 20;
+let gradosFahrenheit = (gradosCelsius * 9) / 5 + 32;
+console.log(gradosCelsius + "°C");
+console.log(gradosFahrenheit + "°F");
