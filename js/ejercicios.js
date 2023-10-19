946c8b479d3987bdf287a20c32a25df409d66ada
nicoIosa <nicolasiosa@hotmail.com>
2023-10-19 09:35:14 -0300

    cambios de variables por molestar

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,17 +1,17 @@
 // Ejercicio 1
 let numero1 = 10;
-let numero2 = 7;
+let numero2 = 3;
 console.log(numero1 + numero2);
 console.log(numero1 - numero2);
 console.log(numero1 / numero2);
 console.log(numero1 * numero2);
 // Ejercicio 2
-let nombre = "Ryan";
-let apellido = "Gosling";
+let nombre = "Max";
+let apellido = "Verstappen";
 let nombreCompleto = nombre + " " + apellido;
 console.log(nombreCompleto);
 // Ejercicio 3
-let gradosCelsius = 20;
+let gradosCelsius = 33;
 let gradosFahrenheit = (gradosCelsius * 9) / 5 + 32;
 console.log(gradosCelsius + "°C");
 console.log(gradosFahrenheit + "°F");
